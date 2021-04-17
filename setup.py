a149c29668013a810d332c0797d9598990bf6de4
Libo Yin <liboyin830@gmail.com>
2021-04-17 23:51:39 +1000

    Added Redis server to dependencies. Unified names.

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setup(
     author_email='liboyin830@gmail.com',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
+    # in pip, Redis server is redis-server; Redis client is redis
+    # in conda, Redis server is redis; Redis client is redis-py
     install_requires=['numpy', 'pandas', 'redis'],
     extras_require={
-        'dev': ['mypy', 'pytest', 'pytest-cov', 'pytest-randomly'],
+        'dev': ['mypy', 'pytest', 'pytest-cov', 'pytest-randomly', 'redis-server'],
     },
 )
